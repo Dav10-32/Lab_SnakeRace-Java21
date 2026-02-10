22f1e68501b7eda197f0bff9ceb6eb7eb4b2f501
Dav10-32 <david.palacios-p@mail.escuelaing.edu.co>
2026-02-10 11:04:47 -0500

    feat: Implemetación punto 2 y 3, con README

--- src/main/java/co/eci/snake/concurrency/SnakeRunner.java
+++ src/main/java/co/eci/snake/concurrency/SnakeRunner.java
@@ -13,32 +13,65 @@ public final class SnakeRunner implements Runnable {
   private final int turboSleepMs = 40;
   private int turboTicks = 0;
 
-  public SnakeRunner(Snake snake, Board board) {
+    private final Object pauseLock = new Object();
+    private volatile boolean paused = false;
+    private volatile boolean actuallyPaused = false;
+
+
+
+    public SnakeRunner(Snake snake, Board board) {
     this.snake = snake;
     this.board = board;
   }
 
-  @Override
-  public void run() {
-    try {
-      while (!Thread.currentThread().isInterrupted()) {
-        maybeTurn();
-        var res = board.step(snake);
-        if (res == Board.MoveResult.HIT_OBSTACLE) {
-          randomTurn();
-        } else if (res == Board.MoveResult.ATE_TURBO) {
-          turboTicks = 100;
-        }
-        int sleep = (turboTicks > 0) ? turboSleepMs : baseSleepMs;
-        if (turboTicks > 0) turboTicks--;
-        Thread.sleep(sleep);
-      }
-    } catch (InterruptedException ie) {
-      Thread.currentThread().interrupt();
-    }
-  }
+    @Override
+    public void run() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
 
-  private void maybeTurn() {
+                synchronized (pauseLock) {
+                    while (paused) {
+                        actuallyPaused = true;
+                        pauseLock.wait();
+                    }
+                    actuallyPaused = false;
+                }
+
+                maybeTurn();
+                var res = board.step(snake);
+
+                if (res == Board.MoveResult.HIT_OBSTACLE) {
+                    randomTurn();
+                } else if (res == Board.MoveResult.ATE_TURBO) {
+                    turboTicks = 100;
+                }
+
+                int sleep = (turboTicks > 0) ? turboSleepMs : baseSleepMs;
+                if (turboTicks > 0) turboTicks--;
+                Thread.sleep(sleep);
+            }
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public boolean isActuallyPaused() {
+        return actuallyPaused;
+    }
+
+    public void pause() {
+        synchronized (pauseLock) {
+            paused = true;
+        }
+    }
+
+    public void resume() {
+        synchronized (pauseLock) {
+            paused = false;
+            pauseLock.notifyAll();
+        }
+    }
+    private void maybeTurn() {
     double p = (turboTicks > 0) ? 0.05 : 0.10;
     if (ThreadLocalRandom.current().nextDouble() < p) randomTurn();
   }
